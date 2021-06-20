0084c91b21ad31ff50c528f918f6977f2b902c48
DjTer3a <44501304+DjTer3a@users.noreply.github.com>
2021-06-20 18:11:41 +0400

    Card.java finalized and comments added

--- cardgame/Card.java
+++ cardgame/Card.java
@@ -1,52 +1,79 @@
-package cardgame;
-
 /**
- * Card - a single playing card
- * @author
- * @version
+ * HW02 Instructions found on moodle
+ * 
+ * Style guidlines URL:-
+ * http://www.cs.bilkent.edu.tr/~adayanik/cs101/practicalwork/styleguidelines.htm
+ * 
+ * 
+ * @author Mostafa Higazy
+ * @version 20/06/2021
  */
+
+ 
+package cardgame;
 public class Card
 {
+    // PROPERTIES
     final String[] SUITS = { "Hearts", "Diamonds", "Spades", "Clubs"};
     final String[] FACES = { "A", "2", "3", "4", "5",
                              "6", "7", "8", "9", "10",
                              "J", "Q", "K"};
-    
     final int NOOFCARDSINSUIT = 13;
-    
-    // properties
     int  cardNo;
     
-    // constructors
+    // CONSTRUCTORS
     public Card( int faceValue, int suit )
     {
         cardNo = faceValue + suit * NOOFCARDSINSUIT;
     }
-    
     public Card( int cardNumber)
     {
         cardNo = cardNumber;
     }
     
+
+    /**
+     * method to get the card number.
+     * @return int cardNo
+     */
     public int getCardNo() {
         return this.cardNo;
     }
     
+    /**
+     * method to get the card face value.
+     * @return int facValue
+     */
     public int getFaceValue()
     {
         return cardNo % NOOFCARDSINSUIT;
     }
     
+
+    /**
+     * method to get the card suit.
+     * @return int suit
+     */
     public int getSuit()
     {
         return cardNo / NOOFCARDSINSUIT;
     }
     
+
+    /**
+     * method to convert anything in the class to string.
+     * @return String 
+     */
     public String toString()
     {
         return FACES[ getFaceValue() ] + " of " + SUITS[ getSuit() ];
     }
     
+
+    /**
+     * method to check if two cards are equal.
+     * @return boolean 
+     */
     public boolean equals( Card c)
     {
         if (this.getFaceValue() == c.getFaceValue() && this.getSuit() == c.getSuit() ) {
@@ -60,15 +87,25 @@ public class Card
         }
     }
     
+
+    /**
+     * method to compare two cards.
+     * @return boolean 
+     */
     public int compareTo( Card c)
     {
         if (this.cardNo == c.cardNo ) {
             return 0;
         }
-
-        //cahnge
         else {
-            return this.cardNo > c.cardNo ? 1 : -1;
+            if(this.cardNo > c.cardNo)
+            {
+                return 1;
+            } 
+            else
+            {
+                return -1;
+            }
         }
     }
 }
